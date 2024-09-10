ccaa1468e09b8ac32796926049c4eb6923fda14c
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2024-09-10 16:00:21 +0800

    Update main.tf - update resource eks name

--- main.tf
+++ main.tf
@@ -172,11 +172,11 @@ data "aws_security_group" "eks_sg" {
   #  id = "sg-08567c7350c264402"
 }
 
-variable "eks_cluster" {
+variable "recovery_eks_cluster" {
 }
 # EKS Cluster
-resource "aws_eks_cluster" "eks_cluster" {
-  name = var.eks_cluster
+resource "aws_eks_cluster" "recovery_eks_cluster" {
+  name = var.recovery_eks_cluster
   version = var.cluster_version
   #  name     = "f92sh02"
   role_arn = data.aws_iam_role.eks_role.arn
@@ -197,7 +197,7 @@ resource "aws_eks_cluster" "eks_cluster" {
 
 # Fargate Profile for kube-system
 resource "aws_eks_fargate_profile" "kube_system_profile" {
-  cluster_name           = aws_eks_cluster.eks_cluster.name
+  cluster_name           = aws_eks_cluster.recovery_eks_cluster.name
   fargate_profile_name   = "kube-system"
   pod_execution_role_arn = data.aws_iam_role.fargate_role.arn
 
@@ -207,12 +207,12 @@ resource "aws_eks_fargate_profile" "kube_system_profile" {
 
   subnet_ids = [data.aws_subnet.subnet_1.id, data.aws_subnet.subnet_2.id]
 
-  depends_on = [aws_eks_cluster.eks_cluster]
+  depends_on = [aws_eks_cluster.recovery_eks_cluster]
 }
 
 # Fargate Profile for platform-service
 resource "aws_eks_fargate_profile" "platform_service_profile" {
-  cluster_name           = aws_eks_cluster.eks_cluster.name
+  cluster_name           = aws_eks_cluster.recovery_eks_cluster.name
   fargate_profile_name   = "platform-service"
   pod_execution_role_arn = data.aws_iam_role.fargate_role.arn
 
@@ -222,12 +222,12 @@ resource "aws_eks_fargate_profile" "platform_service_profile" {
 
   subnet_ids = [data.aws_subnet.subnet_1.id, data.aws_subnet.subnet_2.id]
 
-  depends_on = [aws_eks_cluster.eks_cluster]
+  depends_on = [aws_eks_cluster.recovery_eks_cluster]
 }
 
 # Fargate Profile for velero
 resource "aws_eks_fargate_profile" "velero" {
-  cluster_name           = aws_eks_cluster.eks_cluster.name
+  cluster_name           = aws_eks_cluster.recovery_eks_cluster.name
   fargate_profile_name   = "velero"
   pod_execution_role_arn = data.aws_iam_role.fargate_role.arn
 
@@ -237,7 +237,7 @@ resource "aws_eks_fargate_profile" "velero" {
 
   subnet_ids = [data.aws_subnet.subnet_1.id, data.aws_subnet.subnet_2.id]
 
-  depends_on = [aws_eks_cluster.eks_cluster]
+  depends_on = [aws_eks_cluster.recovery_eks_cluster]
 }
 
 /*
@@ -257,20 +257,20 @@ resource "kubernetes_config_map" "platform_service_config" {
 */
 
 provider "kubernetes" {
-  host                   = aws_eks_cluster.eks_cluster.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority[0].data)
+  host                   = aws_eks_cluster.recovery_eks_cluster.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.recovery_eks_cluster.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.auth.token
 
   # For EKS authentication
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
-    args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.eks_cluster.name]
+    args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.recovery_eks_cluster.name]
   }
 }
 
 data "aws_eks_cluster_auth" "auth" {
-  name = aws_eks_cluster.eks_cluster.name
+  name = aws_eks_cluster.recovery_eks_cluster.name
 }
 
 # Outputs
