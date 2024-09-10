219d7d752e0549ce940f6d2aa3f6575e8fc6963c
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2024-09-10 16:43:30 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -241,7 +241,7 @@ resource "null_resource" "create_oicd" {
       curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
       tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
       sudo mv /tmp/eksctl /usr/local/bin
-      eksctl utils associate-iam-oidc-provider --cluster $cluster_name --approve
+      eksctl utils associate-iam-oidc-provider --cluster ${aws_eks_cluster.recovery_eks_cluster.name} --approve
     EOT
   }
 
