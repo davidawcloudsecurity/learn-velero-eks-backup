43ddb70cef62c9db85bb3ea47fcddceeafc99636
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2024-09-10 16:39:37 +0800

    Update main.tf - install eksctl

--- main.tf
+++ main.tf
@@ -235,6 +235,12 @@ resource "null_resource" "create_oicd" {
 
   provisioner "local-exec" {
     command = <<EOT
+      echo $cluster_name
+      ARCH=amd64
+      PLATFORM=$(uname -s)_$ARCH
+      curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+      tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+      sudo mv /tmp/eksctl /usr/local/bin
       eksctl utils associate-iam-oidc-provider --cluster $cluster_name --approve
     EOT
   }
