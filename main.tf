66754282751f2373770010737620ee97941723ef
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2024-09-11 21:11:46 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -419,7 +419,19 @@ EOF2
       aws eks update-kubeconfig --region ${var.region} --name ${var.recovery_eks_cluster}
       kubectl rollout restart deploy/coredns -n kube-system
       helm install velero vmware-tanzu/velero --create-namespace --namespace velero -f values_recovery.yaml
-      aws eks update-kubeconfig --region ${var.region} --name ${var.primary_cluster}
+      this_account=$(aws sts get-caller-identity --query Account --output text)
+      cluster_name=$(aws eks list-clusters --query clusters[0] --output text)
+      region_code=$(aws ec2 describe-availability-zones --output text --query 'AvailabilityZones[0].[RegionName]')
+      assume_role="project-trust-platform-role"
+      CREDENTIALS=$(aws sts assume-role --role-arn arn:aws:iam::$this_account:role/$assume_role --role-session-name "AssumeRoleSession")
+      export AWS_ACCESS_KEY_ID=$(echo $CREDENTIALS | jq -r '.Credentials.AccessKeyId')
+      export AWS_SESSION_TOKEN=$(echo $CREDENTIALS | jq -r '.Credentials.SessionToken')
+      export AWS_SECRET_ACCESS_KEY=$(echo $CREDENTIALS | jq -r '.Credentials.SecretAccessKey')
+      aws configure --profile assume_role set aws_access_key_id $AWS_ACCESS_KEY_ID
+      aws configure --profile assume_role set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+      aws configure --profile assume_role set aws_session_token $AWS_SESSION_TOKEN
+      aws configure --profile assume_role get region
+      aws --profile assume_role  eks update-kubeconfig --region $region_code --name $cluster_name
       aws eks create-fargate-profile \
       --cluster-name ${var.primary_cluster} \
       --fargate-profile-name velero \
