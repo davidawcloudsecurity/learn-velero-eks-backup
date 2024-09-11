9911736fa86a0a3be45b698b2859614bc442a350
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2024-09-11 15:22:21 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -239,7 +239,7 @@ resource "aws_eks_cluster" "recovery_eks_cluster" {
 }
 
 resource "aws_eks_addon" "example" {
-  cluster_name                = aws_eks_cluster.example.name
+  cluster_name                = aws_eks_cluster.recovery.name
   addon_name                  = "coredns"
   addon_version               = "v1.11.1-eksbuild.11" #e.g., previous version v1.9.3-eksbuild.3 and the new version is v1.10.1-eksbuild.1
   resolve_conflicts_on_update = "OVERWRITE"
