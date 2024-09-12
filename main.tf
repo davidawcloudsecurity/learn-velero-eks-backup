0c476107b5494c0830aabaedc0a324b221795366
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2024-09-12 17:58:17 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -417,6 +417,10 @@ pod:
       value: "fargate"
       effect: "NoSchedule"      
 EOF2
+      if ! kubectl cluster-info > /dev/null 2>&1; then
+        exit 1
+      fi
+      
       if ! $(aws eks list-fargate-profiles --cluster-name ${var.primary_cluster} --query fargateProfileNames --output text | grep velero) > /dev/null 2>&1 && ! kubectl get deploy/velero -n velero > /dev/null 2>&1; then
         echo "Velero namespace/node does not exist, proceeding to create Fargate profile"
         aws eks create-fargate-profile \
