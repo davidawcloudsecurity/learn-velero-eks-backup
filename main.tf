8d13d895301003034eb07a8a83c45547981ba3c8
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2024-09-17 11:06:48 +0800

    Update main.tf - set trigger for backup and recovery arn

--- main.tf
+++ main.tf
@@ -330,6 +330,15 @@ output "velero_policy_arn" {
   value = aws_iam_policy.velero_policy.arn
 }
 
+output "velero-recovery_arn" {
+  value = aws_iam_role.velero-recovery.arn
+}
+
+output "velero-backup_arn" {
+  value = aws_iam_role.velero-backup.arn
+}
+
+
 output "primary_cluster" {
   value = var.primary_cluster
 }
@@ -371,9 +380,11 @@ resource "null_resource" "check_velero_backup_recovery_role" {
   }
 
   # Ensure this only runs when necessary
-  triggers = {
-    velero_backup_recovery_role = aws_iam_policy.velero_policy.arn
-  }
+  triggers = [
+    velero_policy_arn = aws_iam_policy.velero_policy.arn,
+    velero_backup_arn = aws_iam_policy.velero-backup.arn,
+    velero_recovery_arn = aws_iam_policy.velero-recovery.arn,
+  ]
 }
 
 resource "null_resource" "create_oicd" {
