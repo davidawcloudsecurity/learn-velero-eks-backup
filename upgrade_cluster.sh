b525d7e0836c6cd1a3c499fe130341985d0e662a
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2024-09-25 18:39:46 +0800

    Update upgrade_cluster.sh

--- upgrade_cluster.sh
+++ upgrade_cluster.sh
@@ -39,7 +39,6 @@ upgrade_cluster_version() {
 
   if [[ $? -ne 0 ]]; then
     echo "Failed to start cluster upgrade to version $target_version..."
-    echo "Checking nodes..."
     check_node_versions "$target_version"
     # exit 1
   fi
