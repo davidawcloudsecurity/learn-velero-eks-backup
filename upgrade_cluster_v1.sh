116d530b19b7e2d04cd298b691ae26d4985d6e84
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2024-09-27 15:30:37 +0800

    Update upgrade_cluster_v1.sh - added logic flow for deleting pods

--- upgrade_cluster_v1.sh
+++ upgrade_cluster_v1.sh
@@ -124,16 +124,17 @@ delete_pods() {
   
   # Loop through each namespace
   for NAMESPACE in $NAMESPACES; do
-    echo "Deleting all pods in namespace: $NAMESPACE"
-    
-    # Delete all pods in the current namespace
-    kubectl delete pods --all --namespace="$NAMESPACE"
-  
-    if [[ $? -eq 0 ]]; then
-      echo "All pods deleted successfully in namespace: $NAMESPACE"
-    else
-      echo "Failed to delete pods in namespace: $NAMESPACE"
-    fi
+  # Delete all pods in the specified namespace
+  OUTPUT=$(kubectl delete pods --all --namespace="$NAMESPACE" 2>&1)
+  # Check if the command was successful or if no resources were found
+  if echo "$OUTPUT" | grep -q "No resources found"; then
+    echo "No pods found to delete in namespace: $NAMESPACE"
+  elif [[ $? -eq 0 ]]; then
+    echo "All pods deleted successfully in namespace: $NAMESPACE"
+  else
+    echo "Failed to delete pods in namespace: $NAMESPACE"
+    echo "Error details: $OUTPUT"
+  fi   
   done
   echo "All pods in all namespaces have been deleted."
 }
